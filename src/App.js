42c75b9494d6404bc22ae4820f1dd004a6f25901
Anupam740 <anupamkumar740@gmail.com>
2023-05-02 18:03:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React,{useRef} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import "./App.css";
 
 export default function App() {
-  const fileInputref=useRef(null)
-  const handleClick =()=>{
-    fileInputref.current.click()
+  const [index,setIndex]=useState(0)
+  const imgRef =useRef(null)
+
+  
+  const [imgData,SetImgData]=useState("https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022_960_720.jpg")
+  
+  var arr=["https://cdn.pixabay.com/photo/2016/02/17/19/08/lotus-1205631_960_720.jpg","https://cdn.pixabay.com/photo/2014/01/22/19/44/flower-field-250016_960_720.jpg","https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_960_720.jpg","https://cdn.pixabay.com/photo/2013/07/02/22/20/bouquet-142876_960_720.jpg" ]
+  useEffect(()=>{
+    imgRef.current.src=imgData
+
+  })
+  const handleClick=()=>{
+    if(index==arr.length-1){
+      setIndex(0)
+
+    }
+    else{
+      setIndex(index+1)
+      SetImgData(arr[index])
+
+    }
     
+
   }
   return (
-    <div className="container">
-      <input type="file"placeholder="Files" ref={fileInputref}/>
-      <button onClick={handleClick}>Pick a File</button>
+    <div>
+      <img id="img" src="" ref={imgRef} />
+      <button onClick={handleClick}>Click to Change</button>
     </div>
   );
 }
