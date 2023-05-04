bc8ad1cd3d55a0ed4f565e23ffdd14e140cb0c8d
Anupam740 <anupamkumar740@gmail.com>
2023-05-04 20:37:27 +0530

    generating OTP using the given API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React,{useState,useEffect} from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css'
 
-export default function App() {
-  const[data,setData]=useState()
-  async function DogImg(){
-    const response=await fetch('https://dog.ceo/api/breeds/image/random')
-    const dogimage=await response.json()
-    setData(dogimage.message)
-  }
-  useEffect(()=>{
-    DogImg();
-  })
+function App() {
+  const [mobile, setMobile] = useState('');
+  const [otpSent, setOtpSent] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleMobileChange = (event) => {
+    setMobile(event.target.value);
+  };
+
+  const handleSendOtp = (event) => {
+    event.preventDefault();
+    if (/^[6-9]\d{9}$/.test(mobile)) {
+      fetch("https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ mobile: parseInt(mobile) }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            setOtpSent(true);
+            setError(null);
+            setTimeout(() => {
+              setOtpSent(false);
+            }, 120000);
+          } else {
+            setError('Failed to send OTP. Please try again later.');
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          setError('Failed to send OTP. Please check your network connection and try again.');
+        });
+    } else {
+      setError('Please enter a valid Indian mobile number.');
+    }
+  };
+
   return (
-    <div className="dogimg">
-      <img id="img"
-      src={data}/>
+    <div className="App">
+      <form onSubmit={handleSendOtp}>
+        <label htmlFor="mobile">Mobile number:</label>
+        <input
+          type="tel"
+          id="mobile"
+          name="mobile"
+          pattern="[6-9]\d{9}"
+          value={mobile}
+          onChange={handleMobileChange}
+          required
+        />
+        <button type="submit">Send OTP</button>
+      </form>
+      {otpSent && <p>OTP sent successfully. Please check your mobile.</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 }
+
+export default App;
